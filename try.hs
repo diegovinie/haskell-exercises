9ae1dca4331ecc414299d49e43bb26a07198c15a
Diego Viniegra <diego.viniegra@gmail.com>
2019-04-18 10:48:26 -0500

    Recursive functions

--- try.hs
+++ try.hs
@@ -123,3 +123,33 @@ quicksort (a:as) =
   let smallerSorted = quicksort [x | x <- as, x <= a]
       biggerSorted  = quicksort [x | x <- as, x > a]
   in smallerSorted ++ [a] ++ biggerSorted
+
+-- la función fn se aplica a los valores de cada lista
+-- los tipos a y b entran en la función y devuelve c
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ _ [] = []
+zipWith' _ [] _ = []
+zipWith' fn (x:xs) (y:ys) = fn x y : zipWith' fn xs ys
+
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = g
+  where g x y = f y x
+
+flip'' :: (a -> b -> c) -> b -> a -> c
+flip'' f x y = f y x
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' fn (x:xs) = fn x : map fn xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' fn (x:xs)
+  | fn x      = x : filter fn xs
+  | otherwise = filter fn xs
+
+-- máximo divisor div
+largestDivisible :: (Integral a) => a -> a
+largestDivisible div = head (filter fn [100000, 99999..])
+  where fn x = x `mod` div == 0
