ac439e2b5a56d2de121c5003f76ed6fb2f99e6bd
Diego Viniegra <diego.viniegra@gmail.com>
2019-04-22 21:26:52 -0500

    dictionaries

--- modules.hs
+++ modules.hs
@@ -40,3 +40,27 @@ partitioned = partition (`notElem` ['A'..'Z']) "fghYUIbnHJKnerJK"
 
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
 f `on` g = \x y -> f (g x) (g y)
+
+phoneBook =
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs) =
+  if k == key
+  then Just v
+  else findKey key xs
+
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key xs =
+  let matcher = \acc (k, v) -> if key == k then Just v else acc
+  in foldl matcher Nothing xs
+
+fromList' :: (Ord k) => [(k,v)] -> M.Map k v
+fromList' = foldr (\(k,v) acc -> M.insert k v acc) M.empty
